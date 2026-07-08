c2bfd074a9e43ed2c2493b2f6a1ecea838b5d5fc
Antoine <antoine.frau1108@gmail.com>
2026-07-09 00:15:18 +0200

    fix(ci): wrong config (#30)

--- release.config.cjs
+++ release.config.cjs
@@ -5,7 +5,6 @@ module.exports = {
     [
       '@semantic-release/commit-analyzer',
       {
-        preset: 'conventionalcommits',
         releaseRules: [
           { scope: 'benchmark', release: false },
           { scope: 'ci', release: false },
